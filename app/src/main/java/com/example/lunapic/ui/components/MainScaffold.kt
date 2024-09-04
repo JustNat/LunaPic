f5aa584111076a1f8a9ff031ebe9d6c63a56c920
Bruce <gabrielcp4@outlook.com>
2024-09-04 13:32:08 -0300

    useless import removed

--- app/src/main/java/com/example/lunapic/ui/components/MainScaffold.kt
+++ app/src/main/java/com/example/lunapic/ui/components/MainScaffold.kt
@@ -2,7 +2,6 @@ package com.example.lunapic.ui.components
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
@@ -10,7 +9,6 @@ import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
